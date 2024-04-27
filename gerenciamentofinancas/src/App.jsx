91efe79d036ac0dca7ceb8ab54e644dd2af76c9c
FelipeSamassa <137512643+FelipeSamassa@users.noreply.github.com>
2024-04-27 14:02:41 -0300

    atualização

--- gerenciamentofinancas/src/App.jsx
+++ gerenciamentofinancas/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 
+
+// Aplicativo de gerenciamento financeiro pessoal
+
 function App() {
 
   return (
     <>
-      
+      <header>
+        <img src="./src/assets/icons8-financial-64.png" alt="img" />
+        <h1>Gerenciador Financeiro Pessoal</h1>
+      </header>
+      <main>
+
+      </main>
+
     </>
   )
 }
